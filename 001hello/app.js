44efbdb4c50662fd520dd6625bb0891dbede4cb7
Ravi Yadav <rickcarter1806@gmail.com>
2017-10-22 16:43:09 +0530

    First Week

---
+++ 001hello/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end('Hello, World!!\n');
+});
+
+server.listen(8000);
+console.log('Server running at http://localhost:8000');
